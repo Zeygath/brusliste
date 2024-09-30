2a0980745f845027de599cf36e0a2ddbfa497755
Sondre <sondre.haugen99@gmail.com>
2024-09-30 13:53:48 +0200

    added X ixon from lucide-react

--- src/BeverageApp.js
+++ src/BeverageApp.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { PlusCircle, MinusCircle, ShoppingCart, Coffee, UserPlus, ClipboardList } from 'lucide-react';
+import { PlusCircle, MinusCircle, ShoppingCart, Coffee, UserPlus, ClipboardList, X } from 'lucide-react';
 import { Button } from './components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from './components/ui/card';
 import { Input } from './components/ui/input';
