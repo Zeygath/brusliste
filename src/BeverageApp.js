5b09d60dcd1f2cd697c81f53e900ae04c3c3fe4f
Sondre Haugen <sondre.haugen99@gmail.com>
2024-10-08 15:40:01 +0200

    added confirm menu to beverage selection

--- src/BeverageApp.js
+++ src/BeverageApp.js
@@ -338,7 +338,7 @@ const BeverageApp = () => {
           <AlertDialog.Overlay className="bg-black/50 data-[state=open]:animate-overlayShow fixed inset-0" />
           <AlertDialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
             <AlertDialog.Title className="text-green-700 m-0 text-[20px] font-semibold">
-              {selectedAction === 'add' ? 'Add Beverage' : 'Remove Beverage'}
+              {selectedAction === 'add' ? 'Legg til Brus' : 'Fjern Brus'}
             </AlertDialog.Title>
             <AlertDialog.Description className="text-gray-600 mt-4 mb-5 text-[15px] leading-normal">
               <p>Hei {selectedPerson?.name}! Velg hvilken drikke du har tatt:</p>
